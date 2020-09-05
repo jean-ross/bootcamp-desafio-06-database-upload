ab4c1cfd1d40c8e52592fc1ae3c929015f4e8969
Jean Ross <jeancmartins@icloud.com>
2020-09-05 09:34:14 -0300

    Added cors, ORM relations

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -14,6 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
