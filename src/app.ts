1659941e5561973179cb1e6545f4e3d639de5782
Jean Ross <jeancmartins@icloud.com>
2020-09-05 09:59:55 -0300

    Corrected misplaced CORS

--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,8 @@ createConnection();
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
